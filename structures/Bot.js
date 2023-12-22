5896273959995f37be157861df071864062766dc
Khnykin Artem <113325581+Khnykin-Artem@users.noreply.github.com>
2023-12-22 19:10:11 +0300

    create method 'registerCommand' in Bot class

--- structures/Bot.js
+++ structures/Bot.js
@@ -1,4 +1,5 @@
 import { Telegraf } from 'telegraf';
+import { glob } from 'glob';
 
 class Bot {
   constructor(token) {
@@ -7,5 +8,15 @@ class Bot {
 
   async importFile(filePath) {
     return (await import(filePath))?.default;
+  }
+
+  async registerCommands() {
+    const commandFiles = await glob('commands/**/*.js');
+    commandFiles.forEach(async (filePath) => {
+      const command = await this.importFile(filePath);
+      if (!command.name) return;
+      command.initCommand(this.bot);
+      console.log(command);
+    });
   }
 }
