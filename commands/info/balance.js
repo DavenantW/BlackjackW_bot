f2c690c2ae8a85e6b9c73f7a341727addcf7748f
Asych <deadinside228cs@gmail.com>
2024-01-29 16:57:55 +0300

    creat balance command

--- commands/info/balance.js
+++ commands/info/balance.js
@@ -1,3 +1,8 @@
 import Command from '../../structures/Command.js'
 
-export default new Command ("balance", function(ctx){} )
+export default new Command ("balance", async function (ctx){
+  const balance = await fetch('https://blackjackw.com/balance');
+  ctx.reply(`Ваш баланс: ${balance}`)
+} )
+
+
